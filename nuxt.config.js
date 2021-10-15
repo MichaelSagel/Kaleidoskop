96dc3fb7996707f210eff34c91f0943955fced7c
bojar <michael.sagel89@gmail.com>
2021-10-15 12:06:07 +0200

    kaleidoskop

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kaleidoskop',
+    title: 'Kaleidoskop',
     htmlAttrs: {
       lang: 'de'
     },
